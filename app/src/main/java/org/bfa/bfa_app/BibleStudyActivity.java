fdd11b9f77b9412a95254381a1c37871448412ec
fineflour <fineflour@gmail.com>
2018-02-01 15:15:40 -0800

    Added Webview for bible study

--- app/src/main/java/org/bfa/bfa_app/BibleStudyActivity.java
+++ app/src/main/java/org/bfa/bfa_app/BibleStudyActivity.java
@@ -1,41 +1,21 @@
 package org.bfa.bfa_app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
-import android.util.Log;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-
-import static android.content.ContentValues.TAG;
-import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v7.widget.Toolbar;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class BibleStudyActivity extends AppCompatActivity  {
+
     private Toolbar toolbar;
 
 
@@ -46,11 +26,13 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bible_study);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-
+        WebView web_view;
+        web_view = (WebView)findViewById(R.id.web_view);
+        web_view.loadUrl("file:///android_asset/index.html");
     }
      @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,18 +47,13 @@ public class MainActivity extends AppCompatActivity  {
         switch (item.getItemId()) {
             case R.id.action_order:
                 // User chose the "Settings" item, show the app settings UI...
-                Intent intentOrder = new Intent(this, OrderActivity.class);
-                startActivity(intentOrder);
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
                 break;
 
             case R.id.action_download:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
-                break;
-
-            case R.id.action_blble_study:
-                Intent intentStudy = new Intent(this, BibleStudyActivity.class);
-                startActivity(intentStudy);
                 break;
 
             case R.id.action_donation:
